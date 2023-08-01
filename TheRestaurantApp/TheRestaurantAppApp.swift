c2ae7b41805e3c75c7c1c79b9c49b485c11d5499
Naina <ns1388@scarletmail.rutgers.edu>
2023-08-01 12:35:37 -0400

    Initial Commit

---
+++ TheRestaurantApp/TheRestaurantAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TheRestaurantAppApp.swift
+//  TheRestaurantApp
+//
+//  Created by Nayana Sharma on 8/1/23.
+//
+
+import SwiftUI
+
+@main
+struct TheRestaurantAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
